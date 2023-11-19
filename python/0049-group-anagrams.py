7b3f957aef807327dfa857dc5906dcb366444d2b
pochiman <pochiman@yahoo.com>
2023-11-18 22:53:13 -0500

    initial commit

--- python/0049-group-anagrams.py
+++ python/0049-group-anagrams.py
@@ -29,11 +29,11 @@ Output: [["a"]]
 
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        ans = collections.defaultdict(list)
+        res = defaultdict(list)  # mapping charCount to list of Anagrams
 
         for s in strs:
-            count = [0] * 26
+            count = [0] * 26  # a ... z
             for c in s:
                 count[ord(c) - ord("a")] += 1
-            ans[tuple(count)].append(s)
-        return ans.values()
+            res[tuple(count)].append(s)
+        return res.values()
