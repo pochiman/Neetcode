908276ee139f76fee7eba6060bfcabbfe55d899d
pochiman <pochiman@yahoo.com>
2023-12-04 21:27:59 -0500

    initial commit

--- python/0242-valid-anagram.py
+++ python/0242-valid-anagram.py
@@ -31,4 +31,8 @@ class Solution:
         for i in range(len(s)):
             countS[s[i]] = 1 + countS.get(s[i], 0)
             countT[t[i]] = 1 + countT.get(t[i], 0)
-        return countS == countT
+        for i in countS:
+            if countS[i] != countT.get(i, 0):
+                return False
+            
+        return True
