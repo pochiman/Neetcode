ae9618dcbbce6ed73405f027b93c77243c46351f
pochiman <pochiman@yahoo.com>
2023-12-08 22:18:57 -0500

    initial commit

--- python/0036-valid-sudoku.py
+++ python/0036-valid-sudoku.py
@@ -50,22 +50,19 @@ modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invali
 
 class Solution:
     def isValidSudoku(self, board: list[list[str]]) -> bool:
-        cols = collections.defaultdict(set)
         rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
         squares = collections.defaultdict(set)  # key = (r /3, c /3)
 
         for r in range(9):
             for c in range(9):
                 if board[r][c] == ".":
                     continue
-                if (
-                    board[r][c] in rows[r]
-                    or board[r][c] in cols[c]
-                    or board[r][c] in squares[(r // 3, c // 3)]
-                ):
+                if (board[r][c] in rows[r] or 
+                    board[r][c] in cols[c] or 
+                    board[r][c] in squares[(r // 3, c // 3)]):
                     return False
-                cols[c].add(board[r][c])
                 rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
                 squares[(r // 3, c // 3)].add(board[r][c])
-
         return True
