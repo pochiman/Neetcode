f19aaca20feea324bc2ce9d9a6dd56a07daf4599
pochiman <pochiman@yahoo.com>
2025-07-15 00:14:53 -0400

    initial commit

--- python/01_Arrays_&_Hashing/0242-valid-anagram.py
+++ python/01_Arrays_&_Hashing/0242-valid-anagram.py
@@ -4,8 +4,8 @@
 
 Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
-An Anagram is a word or phrase formed by rearranging the letters of a different word 
-or phrase, typically using all the original letters exactly once.
+An anagram is a string that contains the exact same characters as another string, 
+but the order of the characters can be different.
 
 
 
@@ -31,8 +31,8 @@ class Solution:
         for i in range(len(s)):
             countS[s[i]] = 1 + countS.get(s[i], 0)
             countT[t[i]] = 1 + countT.get(t[i], 0)
-        for i in countS:
-            if countS[i] != countT.get(i, 0):
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
                 return False
             
         return True
