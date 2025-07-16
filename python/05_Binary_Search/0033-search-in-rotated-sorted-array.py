84b1519c53d08e136cde16ba4024cef9fe71dad4
pochiman <pochiman@yahoo.com>
2025-07-16 17:22:15 -0400

    initial commit

--- python/05_Binary_Search/0033-search-in-rotated-sorted-array.py
+++ python/05_Binary_Search/0033-search-in-rotated-sorted-array.py
@@ -35,14 +35,13 @@ Output: -1
 """
 
 class Solution:
-    def search(self, nums: list[int], target: int) -> int:
+    def search(self, nums: List[int], target: int) -> int: # type: ignore
         l, r = 0, len(nums) - 1
 
         while l <= r:
             mid = (l + r) // 2
             if target == nums[mid]:
                 return mid
-
             # left sorted portion
             if nums[l] <= nums[mid]:
                 if target > nums[mid] or target < nums[l]:
@@ -55,5 +54,4 @@ class Solution:
                     r = mid - 1
                 else:
                     l = mid + 1
-        
         return -1
