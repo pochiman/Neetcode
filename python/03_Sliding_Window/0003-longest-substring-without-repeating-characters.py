3f8ad4b614be8eecaec4f8ef3142ef102f8a0c43
pochiman <pochiman@yahoo.com>
2025-07-16 12:20:50 -0400

    initial commit

--- python/03_Sliding_Window/0003-longest-substring-without-repeating-characters.py
+++ python/03_Sliding_Window/0003-longest-substring-without-repeating-characters.py
@@ -2,7 +2,7 @@
 
 3. Longest Substring Without Repeating Characters
 
-Given a string s, find the length of the longest substring without repeating characters.
+Given a string s, find the length of the longest substring without duplicate characters.
 
 
 
