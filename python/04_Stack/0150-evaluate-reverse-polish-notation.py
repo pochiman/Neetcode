48df9e35d0ea5f2e37f80f96434bdaa5053f9f88
pochiman <pochiman@yahoo.com>
2025-07-16 13:47:06 -0400

    initial commit

--- python/04_Stack/0150-evaluate-reverse-polish-notation.py
+++ python/04_Stack/0150-evaluate-reverse-polish-notation.py
@@ -45,7 +45,7 @@ Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
 """
 
 class Solution:
-    def evalRPN(self, tokens: list[str]) -> int:
+    def evalRPN(self, tokens: List[str]) -> int: # type: ignore
         stack = []
         for c in tokens:
             if c == "+":
