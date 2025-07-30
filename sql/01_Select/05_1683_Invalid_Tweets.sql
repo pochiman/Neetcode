2ea5f1d4ac143a4b834cadb107e89c1c37f44cab
pochiman <pochiman@yahoo.com>
2025-07-29 23:21:41 -0400

    initial commit

--- sql/01_Select/05_1683_Invalid_Tweets.sql
+++ sql/01_Select/05_1683_Invalid_Tweets.sql
@@ -2,6 +2,13 @@
 1683. Invalid Tweets
 """
 
+-- Solution 1:
 SELECT tweet_id 
 FROM Tweets 
 WHERE LENGTH(content) > 15
+
+
+-- Solution 2:
+SELECT tweet_id 
+FROM Tweets 
+WHERE CHAR_LENGTH(content) > 15
